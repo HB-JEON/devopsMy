53141c6c0641b3880ef67f5b7ede326d0dfd8873
HB-JEON <wjsgusqls@nate.com>
2025-06-12 16:02:13 +0900

    2025-06-12 CI / CD START

---
+++ src/main/java/com/sist/web/entity/TourEntity.java
@@ -0,0 +1,29 @@
+package com.sist.web.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/*
+	NO           NOT NULL NUMBER        
+	CONTENT_ID            NUMBER        
+	CONTENT_TYPE NOT NULL NUMBER        
+	TITLE        NOT NULL VARCHAR2(300) 
+	ADDR         NOT NULL VARCHAR2(300) 
+	AREACODE              NUMBER        
+	SIGUNGUCODE           NUMBER        
+	IMG                   VARCHAR2(500) 
+	CAT1         NOT NULL VARCHAR2(200) 
+	CAT2         NOT NULL VARCHAR2(200) 
+	CAT3         NOT NULL VARCHAR2(500) 
+	MAPX                  NUMBER(10,6)  
+	MAPY                  NUMBER(10,6)  
+	OVERVIEW              CLOB      
+ */
+@Entity(name = "tour")
+@Data
+public class TourEntity {
+	@Id
+	private int no;
+	private int content_id, content_type, areacode, sigungucode;
+}
